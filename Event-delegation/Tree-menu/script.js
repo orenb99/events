1a0fa99e0b01159bf4952ab3aca8eb956a007bb0
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-26 22:04:39 +0200

    tree task done

--- Event-delegation/Tree-menu/script.js
+++ Event-delegation/Tree-menu/script.js
@@ -1,5 +1,17 @@
-const lists=document.getElementsByTagName("LI");
+const lists=document.getElementsByTagName("li");
 for(let list of lists){
-    if(list.hasChildNodes())
-        alert(list.innerText);
+    const span=document.createElement("span");
+    list.prepend(span);
+    span.prepend(span.nextSibling);
+}
+const tree=document.getElementById("tree");
+tree.addEventListener("click",function (event){
+target=event.target;
+if(target.tagName==="SPAN")
+    collapse(target);
+});
+
+function collapse(span){
+    if(span.parentNode.querySelector("ul"))
+        span.parentNode.querySelector("ul").hidden=!span.parentNode.querySelector("ul").hidden;
 }
